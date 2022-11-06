4b3c2aad6186a042485b69f30652ce8f98a9fc91
Krestfell <krestfell@gmail.com>
2022-11-06 00:04:43 -0500

    h

--- Java/class/varDemo/Addition.java
+++
@@ -1,9 +0,0 @@
-public class Addition {
-    public static void main(String[] args) {
-        int num1 = 80, num2 = 100, sum;
-
-        sum = num1 + num2;
-
-        System.out.println("80 + 100 = " + sum);
-    }
-}
